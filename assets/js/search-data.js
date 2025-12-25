df83e92535471abd138c55186d7e5fa4d18f41d8
thermofluidslab <136124312+thermofluidslab@users.noreply.github.com>
2025-12-25 10:45:35 +0000

    Deploying to gh-pages from @ thermofluidslab/thermofluidslab.github.io@d6eaeb523b25583f9aeafce5f9282babdec05691 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,8 +14,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-home",
-    title: "Home",
+    id: "nav-about",
+    title: "about",
     section: "Navigation menu",
     handler: () => {
       window.location.href = "/";
